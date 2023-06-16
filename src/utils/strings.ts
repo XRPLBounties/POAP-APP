f1a6db6559ece87d3fbbec10157a5b5eafa58a7c
Riku <riku.block@gmail.com>
2023-06-16 23:51:57 +0200

    add webpack

--- src/utils/strings.ts
+++ src/utils/strings.ts
@@ -1,4 +1,4 @@
-// import { isValidAddress } from "xrpl";
+import { isValidAddress } from "xrpl";
 
 export const shortenAddress = (address?: string, chars: number = 4): string => {
   if (!address) {
@@ -7,9 +7,9 @@ export const shortenAddress = (address?: string, chars: number = 4): string => {
   if (address.length <= 2 * chars) {
     return address;
   }
-  // if (!isValidAddress(address)) {
-  //   throw Error(`Invalid address '${address}'`);
-  // }
+  if (!isValidAddress(address)) {
+    throw Error(`Invalid address '${address}'`);
+  }
   const start = address.slice(0, chars);
   const end = address.slice(-chars);
   return `${start}...${end}`;
