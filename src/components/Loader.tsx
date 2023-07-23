a22376081d2c5289caa6657cc090d2fa2868a221
Riku <riku.block@gmail.com>
2023-07-23 12:02:54 +0200

    make loader text customizable

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -2,13 +2,16 @@ import Box from "@mui/material/Box";
 import CircularProgress from "@mui/material/CircularProgress";
 import Typography from "@mui/material/Typography";
 
-function Loader() {
+type LoaderProps = {
+  text?: string;
+};
+
+function Loader(props: LoaderProps) {
+  const { text } = props;
   return (
     <Box sx={{ display: "block", padding: "1rem 0rem", textAlign: "center" }}>
       <CircularProgress color="primary" />
-      <Typography variant="body1">
-        Loading
-      </Typography>
+      <Typography variant="body1">{text ?? "Loading"}</Typography>
     </Box>
   );
 }
