27615f606a45da7527872c7a3b7e9c9653a95420
Riku <riku.block@gmail.com>
2023-09-04 23:50:14 +0200

    update admin users page

--- src/pages/AdminUsersPage.tsx
+++ src/pages/AdminUsersPage.tsx
@@ -7,28 +7,68 @@ import { Box, Button } from "@mui/material";
 
 import API from "apis";
 import { useWeb3 } from "connectors/context";
-import { DialogIdentifier, Event } from "types";
-import { activeDialogAtom } from "states/atoms";
-import EventTable, { type EventTableRow } from "components/EventTable";
 import ContentWrapper from "components/ContentWrapper";
 import { useAuth } from "components/AuthContext";
+import type { User } from "types";
 
 function AdminUsersPage() {
   const { isActive, networkId } = useWeb3();
   const { isAuthenticated, jwt, permissions } = useAuth();
-  const [data, setData] = React.useState<Event[]>();
-  const [slots, setSlots] = React.useState<{ used: number; max: number }>();
-  const [activeDialog, setActiveDialog] = useAtom(activeDialogAtom);
+  const [data, setData] = React.useState<User[]>();
   const { enqueueSnackbar } = useSnackbar();
 
   const isAuthorized = React.useMemo(() => {
     return isAuthenticated && permissions.includes("admin");
   }, [isAuthenticated, permissions]);
 
+  React.useEffect(() => {
+    let mounted = true;
+
+    const load = async () => {
+      try {
+        if (jwt) {
+          const users = await API.users.getOrganizers(jwt);
+          console.log(users); // TODO
+
+          if (mounted) {
+            setData(users);
+          }
+        }
+      } catch (err) {
+        console.debug(err);
+        if (mounted) {
+          setData(undefined);
+        }
+        if (axios.isAxiosError(err)) {
+          enqueueSnackbar(
+            `Failed to load users data: ${err.response?.data.error}`,
+            {
+              variant: "error",
+            }
+          );
+        } else {
+          enqueueSnackbar("Failed to load users data", {
+            variant: "error",
+          });
+        }
+      }
+    };
+
+    if (isAuthorized) {
+      load();
+    } else {
+      setData(undefined);
+    }
+
+    return () => {
+      mounted = false;
+    };
+  }, [isAuthorized, jwt]);
+
   return (
     <ContentWrapper
       title="Admin Panel"
-      isLoading={false}
+      isLoading={!Boolean(data)}
       isAuthorized={isAuthorized}
     >
       Users
