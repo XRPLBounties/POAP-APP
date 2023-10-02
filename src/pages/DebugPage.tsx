86e2bdf6d45c114274f530a5311b98b33279e51f
Riku <riku.block@gmail.com>
2023-10-02 12:34:47 +0200

    add debug clear storage

--- src/pages/DebugPage.tsx
+++ src/pages/DebugPage.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 
+import { Box, Button, Stack } from "@mui/material";
+
+const clearStorage = async () => {
+  console.log("Clearing storages");
+  window.localStorage.clear();
+  window.sessionStorage.clear();
+};
+
 function DebugPage() {
-  return <React.Fragment></React.Fragment>;
+  return (
+    <Box>
+      <Stack sx={{ mb: "1rem" }} direction="row" spacing={1}>
+        <Button onClick={() => clearStorage()} variant="contained">
+          Clear Storage
+        </Button>
+      </Stack>
+    </Box>
+  );
 }
 
 export default DebugPage;
